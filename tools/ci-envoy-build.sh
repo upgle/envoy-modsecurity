216936a77efc601cbeebb6ed1da3fe1c8a76c37a
Seonghyun Oh <seonghyunoh@gmail.com>
2026-07-18 19:07:46 +0900

    ci: print ModSecurity configure diagnostics

--- tools/ci-envoy-build.sh
+++ tools/ci-envoy-build.sh
@@ -11,5 +11,13 @@ git config --global --add safe.directory "${PWD}/third_party/modsecurity"
 git config --global --add safe.directory "${PWD}/third_party/coreruleset"
 bazel --version
 df --human-readable
-bazel build //third_party:libmodsecurity
+if ! bazel build --sandbox_debug //third_party:libmodsecurity; then
+  find /build/.cache/bazel \
+    -type f \
+    -name config.log \
+    -path '*libmodsecurity*' \
+    -print \
+    -exec tail --lines=200 {} \;
+  exit 1
+fi
 make check
